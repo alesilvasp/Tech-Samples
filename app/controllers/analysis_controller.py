92cc418f874425a434a4cfdf05163ee10ab01607
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2021-12-08 20:33:31 -0300

    feat: analysis controller with no token auth

--- app/controllers/analysis_controller.py
+++ app/controllers/analysis_controller.py
@@ -1,8 +1,148 @@
 
-def create_analysis(): ...
+from datetime import datetime
+from flask import jsonify, request, current_app
+from sqlalchemy.exc import IntegrityError
 
-def read_analysis(): ...
+from app.models.analysis_model import AnalysisModel
+from app.models.user_analyst_model import UserAnalystModel
+from app.exceptions.analysis_exceptions import InvalidKeysError, MissingKeysError, TypeError, ForeignKeyNotFoundError
 
-def read_by_id_analysis(id: int): ...
+def create_analysis():
+    data = request.json
+    session = current_app.db.session
 
-def update_analysis(id: int): ...
+    try:
+        AnalysisModel.check_data(**data)
+    except (InvalidKeysError, MissingKeysError, TypeError, ForeignKeyNotFoundError) as err:
+        return err.message
+
+    analysis = AnalysisModel(**data, is_concluded=False)
+
+    try:
+        session.add(analysis)
+        session.commit()
+    except IntegrityError as err:
+        return {'error': f'Analysis with batch id {analysis.batch} already exists.'}, 409
+
+    return jsonify(analysis), 201    
+
+def read_analysis():
+    data = request.json
+
+    if not 'analyst_id' in data:
+        return MissingKeysError().message
+
+    for key in data:
+        if key != 'analyst_id':
+            return InvalidKeysError().message
+
+    analyst_id = data['analyst_id']
+    
+    if type(analyst_id) != int:
+        return TypeError().message
+    
+    analyst = UserAnalystModel.query.filter_by(id=analyst_id).first()
+
+    if not analyst:
+        return {'error': f'Analyst with id {analyst_id} was not found.'}, 404
+
+    analysis = AnalysisModel.query.filter_by(analyst_id=analyst_id).all()
+    concluded_analysis = list()
+    pending_analysis = list()
+
+    if analysis != None:
+        for a in analysis:
+            if a.is_concluded:
+                concluded_analysis.append(a)
+            else:
+                pending_analysis.append(a)
+
+    return jsonify({
+        'concluded_analysis': concluded_analysis,
+        'pending_analysis': pending_analysis
+    })
+
+def read_by_id_analysis(id: int):
+    data = request.json
+
+    if not 'analyst_id' in data:
+        return MissingKeysError().message
+
+    for key in data:
+        if key != 'analyst_id':
+            return InvalidKeysError().message
+
+    analyst_id = data['analyst_id']
+    
+    if type(analyst_id) != int:
+        return TypeError().message
+    
+    analyst = UserAnalystModel.query.filter_by(id=analyst_id).first()
+
+    if not analyst:
+        return {'error': f'Analyst with id {analyst_id} was not found.'}, 404
+
+    analysis = AnalysisModel.query.filter_by(id=id).first()
+
+    if not analysis:
+        return {'error': f'Analysis with id {id} was not found.'}, 404
+
+    if analysis.analyst_id == analyst_id:
+        return jsonify(analysis)
+    
+    return {'error': f'Analyst with id {analyst_id} has no access to analysis {id}'}, 401
+
+def update_analysis(id: int, analyst_id: int):
+    data = request.json
+    session = current_app.db.session
+
+    try:
+        valid_keys = [
+	        'made',
+	        'category',
+	        'class_id',
+	        'analyst_id',
+            'is_concluded'
+        ]
+
+        for key in data:
+            if not key in valid_keys:
+                raise InvalidKeysError()
+
+            if key in ['category'] and type(data[key]) != str:
+                    raise TypeError()
+            
+            if key in ['class_id', 'analyst_id'] and type(data[key]) != int:
+                    raise TypeError()
+            
+            if key in ['made']:
+                try:
+                    data[key] = datetime.strptime(data[key], '%d-%m-%Y')
+                except:
+                    raise TypeError()       
+
+            if key in ['is_concluded'] and type(data[key]) != bool:
+                raise TypeError()
+
+    except (InvalidKeysError, TypeError, ForeignKeyNotFoundError) as err:
+        return err.message
+
+    analysis = AnalysisModel.query.filter_by(id=id).first()
+
+    if not analysis:
+        return {'error': f'Analysis with id {id} was not found.'}, 404
+    
+    analyst = UserAnalystModel.query.filter_by(id=analyst_id).first()
+
+    if not analyst:
+        return {'error': f'Analyst with id {analyst_id} was not found.'}, 404
+    
+    if analysis.analyst_id != analyst.id: 
+        return {'error': f'Analyst with id {analyst_id} has no access to analysis {id}'}, 401
+    
+    for key in data:
+        setattr(analysis, key, data[key])
+    
+    session.commit()
+    
+    return jsonify(analysis)
--- app/models/analysis_model.py
+++ app/models/analysis_model.py
@@ -3,6 +3,10 @@ from dataclasses import dataclass
 from sqlalchemy.orm import validates
 from datetime import datetime
 
+from app.exceptions.analysis_exceptions import InvalidKeysError, MissingKeysError, TypeError, ForeignKeyNotFoundError
+from app.models.classes_model import ClassModel
+from app.models.user_analyst_model import UserAnalystModel
+
 
 @dataclass
 class AnalysisModel(db.Model):
@@ -23,6 +27,46 @@ class AnalysisModel(db.Model):
     class_id = db.Column(db.Integer, db.ForeignKey('classes.id'))
 
     analyst_id = db.Column(db.Integer, db.ForeignKey('users_analysts.id'))
+
+    @classmethod
+    def check_data(cls, **data):
+        valid_keys = [
+            'batch',
+	        'made',
+	        'category',
+	        'class_id',
+	        'analyst_id',
+        ]
+
+        for key in data:
+            if not key in valid_keys:
+                raise InvalidKeysError()
+
+            if key in ['category', 'batch']:
+                if type(data[key]) != str:
+                    raise TypeError()
+            
+            if key in ['class_id', 'analyst_id']:
+                if type(data[key]) != int:
+                    raise TypeError()
+            
+            if key in ['made']:
+                try:
+                    data[key] = datetime.strptime(data[key], '%d-%m-%Y')
+                except:
+                    raise TypeError()
+
+        for key in valid_keys:
+            if not key in data:
+                raise MissingKeysError()
+        
+        analysis_class = ClassModel.query.filter_by(id=data['class_id']).first()
+        analysis_analyst = UserAnalystModel.query.filter_by(id=data['analyst_id']).first()
+
+        if not analysis_analyst or not analysis_class:
+            raise ForeignKeyNotFoundError()
+        
+
 
     @validates('category')
     def validate_values(self, key, value: str):
