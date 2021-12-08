7097faab7b01aa0bbeaeff820c28d348772ef17b
@alesilvasp <alesilvasp@gmail.com>
2021-12-08 10:21:57 -0300

    fix: deleted table user_analyst to create one table named users, fix column name unit, remove unique constraint from name in types table

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from app.routes.user_admin_blueprint import bp_user_admin
-from app.routes.login_blueprint import bp_login
+# from app.routes.user_admin_blueprint import bp_user_admin
+# from app.routes.login_blueprint import bp_login
 
 
 def init_app(app: Flask):
 
-    app.register_blueprint(bp_user_admin)
-    app.register_blueprint(bp_login)
+    # app.register_blueprint(bp_user_admin)
+    # app.register_blueprint(bp_login)
+    ...
