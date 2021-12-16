2ac3d90a934f79d5e0c0837d9ab556311210310f
Eduardo Parraga <eduardo.parraga95@gmail.com>
2021-12-16 14:33:15 -0300

    feat: scheduler timer changed

--- app/configs/scheduler/__init__.py
+++ app/configs/scheduler/__init__.py
@@ -6,7 +6,7 @@ from app.configs.scheduler.email_report_job import send_email_report
 
 def init():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(func=send_email_report, trigger='interval', hours=24)
+    scheduler.add_job(func=send_email_report, trigger='interval', hours=2)
     scheduler.start()
 
     atexit.register(lambda: scheduler.shutdown())
